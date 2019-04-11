6e09c6b060edab92cdb289f6d3041ca4d25bfb34
Sharon Kawale <c3297125@uon.edu.au>
2019-04-11 03:03:42 +0000

    successfully transformed input message into  ascii codes

--- main.c
+++ main.c
@@ -3,16 +3,21 @@
 //THE ENCRYPTION STYLE IS A ROTATION CYPHER
 
 int main() {
-	FILE *input;
-	input = fopen("alphabet.txt", "r");      //read input(alphabet) and assign to pointer
-  char letter;
-  int num ;
+    /*FILE *input_a;   //this is a pointer to the alphabet input text
+	input_a = fopen("alphabet.txt", "r");   */   //read input(alphabet) and assign to pointer
+	
+	FILE *input_b;   //this is a pointer to the message input text
+	input_b = fopen("ascii_alpha.txt", "r");   //open and read the message text
+	
+	
+	char letter;
+	int num ;
   
-  
-	while(feof(input) ==0 ) {               
+  //this is a loop to assign alphabet to numbers       
+	/*while(feof(input_a) ==0 ) {        
 	    
 	    //read a character
-	    fscanf(input, "%c", &letter);
+	    fscanf(input_a, "%c", &letter);
 	    //assign message letter to a number
 	   if (letter >= 'A' && letter <= 'Z') {
 	  num = letter - 'A';
@@ -22,7 +27,28 @@ int main() {
 	    
 	    num = letter - 'a';
 	    printf("%d\n", num);
+	} */
+	
+	//this is a loop to assign each message letter to ascii characters
+	while(feof(input_b) ==0 ) {               
+	    
+	    //read a character
+	    fscanf(input_b, "%c", &letter);
+	    //assign message letter to a number
+	   if (letter >= 'A' && letter <= 'Z') {
+	  num = letter;
+	  printf("%d, ", num);
+  
+	} else if (letter >= 'a' && letter <= 'z') {
+	    
+	    num = letter;
+	    printf("%d, ", num);
+	}  else {
+	    /*num = letter;
+	    num = num - num + 32;*/
+	    printf("%c", letter);
 	}
+	
 	   
 	
  
