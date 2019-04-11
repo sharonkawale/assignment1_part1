99f0bdd242ecfbb26b83990df97f76513378471a
Sharon Kawale <c3297125@uon.edu.au>
2019-04-11 06:05:15 +0000

    successfully encrypted small, all caps text

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ int main() {
 	FILE *input_b;   //this is a pointer to the message input text
 	input_b = fopen("ascii_alpha.txt", "r");   //open and read the message text
 	
+	FILE *input_a;   //this is a pointer to the alphabet input text
+	input_a = fopen("alphabet.txt", "r");   //read input(alphabet) and assign to pointer
+	
 	char letter, e_l;
-
-	int e_n, k = 2, m; 
+	int k=2, m, e_n;
 	
 	
-
+  
   
 	//this is a loop to assign each message letter to ascii characters
 	while(feof(input_b) ==0 ) {               
@@ -20,28 +22,28 @@ int main() {
 	    //read a character
 	    fscanf(input_b, "%c", &letter);
 	    //assign message letter to a number
+	    
+	    m = letter;
+	    
 	   if (letter >= 'A' && letter <= 'Z') {
-	       m = letter;
 	       
-	       e_n = ((m+k)%26);
+	       e_n = ((m+k));
 	       e_l = e_n;
-	       printf("%c", e_l);
+	       printf("%c", e_n);
 	    } 
 	       
 	    else if (letter >= 'a' && letter <= 'z') {
 	    
-	    m = letter;
+	    
 	    m = m - 32;   //make letter capital
-	    e_n = ((m+k)%26);
+	    e_n = ((m+k));
 	    e_l = e_n;
-	    printf("%c", e_l); 
+	    printf("%c", e_n); 
 	    
 	    }  
 	    
 	    else {
-	   printf("%c", letter);
-	}
-	    
+	   printf("%c", letter);  }
 	    
 	}
 	
