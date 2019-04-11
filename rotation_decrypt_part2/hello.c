40d4482ac2404ba6e81a240bef90e16e5163aca9
Sharon Kawale <c3297125@uon.edu.au>
2019-04-11 09:26:35 +0000

    attempted decryption without key and decryption with key

---
+++ rotation_decrypt_part2/hello.c
@@ -0,0 +1,47 @@
+/* Hello World program */
+
+#include<stdio.h>
+
+int main()
+{
+    
+    FILE *input;
+    input = fopen("decrypt_1.txt", "r");
+    
+   
+  char message;
+    int c, d;
+    float nth_l[25], i;
+    
+    
+   
+    
+    
+    while(feof(input) == 0){
+        fscanf(input, "%c", &message);
+     
+      c = message;
+       //this makes input letters ascii numbers
+      o = c - 65;
+      
+      for(i = 0; i <25; i++) {
+        k =  c - 65 - nth_l[i];
+      }
+    
+     if (c >= 65 && c <=90) {
+           d = (c-65)%26 + 65 - 26;
+           
+           
+           printf("%c", d);
+       }
+       else {
+           printf("%c", message);
+       } }
+       
+    
+    
+      
+         
+     
+
+}
