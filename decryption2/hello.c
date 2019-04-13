f8adb036b9f21ff51e03f308593612beca4db8cd
Sharon Kawale <c3297125@uon.edu.au>
2019-04-13 08:00:11 +0000

    attempting to loop key decryption

--- decryption2/hello.c
+++ decryption2/hello.c
@@ -9,30 +9,35 @@ int main()
     
    //declare variables
   char message;
-    int c, d;
+    int c, d, k = 0;
 
 
     //scan and assign letters
-    while(feof(input) == 0){
-        fscanf(input, "%c", &message);
-     
-      c = message;  //make inputs ascii numbers
-      
     
-     if (c >= 65 && c <=90) {
-           d = (c-14-65)%26 + 65  ;  //key = 7
+    while(k >=0  && k <=25){
+        printf("message with %d: \n", k);
+        
+        while(feof(input) == 0){
+        fscanf(input, "%c", &message);
+        
+        c = message;  //make inputs ascii numbers
+        
+        if (c >= 65 && c <=90) {
+           d = (c-k-65)%26 + 65  ;  //key = 14
            if (d<65) {
-               d = d + 26;
-           }
-           
+               d = d + 26;  }
            
            printf("%c", d);
        }
        else {
-           printf("%c", message);
+           printf("%c", message); }
    }
-
-         }
+   
+  
+    
+       k++;    }
+         
+         
      }
 
  
