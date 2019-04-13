3d86d193f541b2149042fd6fd70349bc95561df5
Sharon Kawale <c3297125@uon.edu.au>
2019-04-13 02:34:21 +0000

    the key for the decryption was solved however need to solve looping issue

---
+++ decryption2/hello.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    //open and read input file
+    FILE *input;
+    input = fopen("decrypt_1.txt", "r");
+    
+   //declare variables
+  char message;
+    int c, d;
+
+
+    //scan and assign letters
+    while(feof(input) == 0){
+        fscanf(input, "%c", &message);
+     
+      c = message;  //make inputs ascii numbers
+      
+    
+     if (c >= 65 && c <=90) {
+           d = (c-7-65)%26 + 65  ;  //key = 7
+           if (d>90) {
+               d = d - ;
+           }
+           
+           
+           printf("%c", d);
+       }
+       else {
+           printf("%c", message);
+   }
+
+         }
+     }
+
+ 
+
+
+
