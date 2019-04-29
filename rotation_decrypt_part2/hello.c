dadf9b720e077353a1b0f710f463b4f9d68f1713
Sharon Kawale <c3297125@uon.edu.au>
2019-04-29 05:39:17 +0000

    final decryption through rotation. with a given key

--- rotation_decrypt_part2/hello.c
+++ rotation_decrypt_part2/hello.c
@@ -1,48 +1,53 @@
-/* Hello World program */
-//rotation decryption without a given key
 
-#include<stdio.h>
+//rotation decryption with a given key
+//this is the final decyrption code
 
-int main()
-{
-    
-    FILE *input;
-    input = fopen("decrypt_1.txt", "r");
-    
+#include <stdio.h>
+
+
+int main() {
    
-  char message;
-    int c, d;
-    float nth_l[25], i;
-    
-    
-   
-    
-    
-    while(feof(input) == 0){
-        fscanf(input, "%c", &message);
-     
-      c = message;
-       //this makes input letters ascii numbers
-      o = c - 65;
-      
-      for(i = 0; i <25; i++) {
-        k =  c - 65 - nth_l[i];
-      }
-    
-     if (c >= 65 && c <=90) {
-           d = (c-65)%26 + 65 - 26;
-           
-           
-           printf("%c", d);
-       }
-       else {
-           printf("%c", message);
-       } }
-       
-    
-    
-      
-         
-     
+	FILE *input;   //this is a pointer to the encrypted input text
+	input = fopen("input.txt", "r");   //open and read the text
+	
+	
+	char inchar;
+	int k = 7 , outchar;
+	
+  
+	//this loop goes through each character until it reaches the end
+	while(feof(input) ==0 ) {               
+	    
+	   
+	    fscanf(input, "%c", &inchar);   //read each character and assign to variable
+	  
+	    
+	   if (inchar >= 'A' && inchar <= 'Z') {
+	      
+	      outchar = (inchar-k-65)%26 + 65;  //if input is a capital letter proceed to decrypt
+	       if (outchar < 65) {
+	           outchar = outchar + 26;
+	       }
+	          printf("%c", outchar); //c changes the number to a character
+     } 
 
+	       
+	  else if (inchar >= 'a' && inchar <= 'z') {
+	      inchar = inchar - 32;             //if input is lower case, make it uppercase and then proceed to encyppt 
+	      outchar = (inchar-k-65 )%26 + 65;
+	     
+	     if (outchar < 65) {
+	           outchar = outchar + 26;
+	       }
+	          printf("%c", outchar); }
+	     
+	    
+	    else {
+	   
+	   printf("%c", inchar);  }
+	    
+	}
+	
+	
+	return 0;
 }
